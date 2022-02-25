501a1452639fa0679961124b4c19a305442b12ea
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-02-24 23:58:06 -0600

    Add Views to ComposeActivity

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,14 +1,62 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class ComposeActivity extends AppCompatActivity {
+
+    public static final int MAX_TWEET_LENGTH = 140;
+
+
+    EditText etCompose;
+    Button btnTweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet = findViewById(R.id.btnTweet);
+
+        //Set click listener on button
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Check if the tweet is too short or too long
+                String tweetContent = etCompose.getText().toString();
+                if (tweetContent.isEmpty()){
+                    Snackbar snackbar = Snackbar.make( btnTweet ,"Sorry, your tweet cannot be empty",Snackbar.LENGTH_SHORT);
+                    snackbar.setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(getApplicationContext(),"Undo action",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    snackbar.show();
+                    //Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (tweetContent.length() > MAX_TWEET_LENGTH){
+                    
+                    //Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+                //Make an APT call to Twitter to publish the tweet
+            }
+        });
+
+
     }
 }
