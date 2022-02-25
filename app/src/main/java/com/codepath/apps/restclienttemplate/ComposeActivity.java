4fa6cf7013ab3a8a9bada686b1535f471f645427
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-02-24 22:42:04 -0600

    Create ComposeActivity

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,4 +1,14 @@
 package com.codepath.apps.restclienttemplate;
 
-public class ComposeActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+    }
 }
